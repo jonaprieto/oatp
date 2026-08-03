b9b17cc2e3d89eb3d468aea027c3b1fcaa360dcd
Jonathan Cubides <prieto.jona@gmail.com>
2026-08-03 07:26:12 -0500

    chore: standardize source headers
    Use the ecosystem author identity and header convention across all Lean sources.

--- OATP/Events.lean
+++ OATP/Events.lean
@@ -1,6 +1,7 @@
 /-
-Copyright (c) 2026 Jonathan Cubides. All rights reserved.
+Copyright (c) 2026 Jonathan Prieto-Cubides. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Jonathan Prieto-Cubides
 -/
 
 import OATP.Core
--- test/Tests.lean
+++ test/Tests.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2026 Jonathan Prieto-Cubides. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Jonathan Prieto-Cubides
+-/
+
 import OATP
 import OATP.TPTP
 import OATP.SystemOnTPTP
