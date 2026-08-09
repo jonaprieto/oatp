deefcaaf8f9a6b375363e178ee6e7258e81b313e
Jonathan Cubides <prieto.jona@gmail.com>
2026-08-08 22:02:19 -0500

    build: update terminal to v0.3.0

--- lakefile.lean
+++ lakefile.lean
@@ -19,7 +19,7 @@ require «termcolor» from git
 
 require «termcolor-terminal» from git
   "https://github.com/jonaprieto/lean-termcolor-terminal.git"
-  @ "v0.2.0"
+  @ "v0.3.0"
 
 require argus from git
   "https://github.com/jonaprieto/lean-argus.git"
