49d2b1889c7bb8131f1a6b3b7dc1bb7d1b43d5a1
Jonathan Cubides <prieto.jona@gmail.com>
2026-08-13 11:43:49 -0500

    chore(release): prepare v0.7.5

--- lakefile.lean
+++ lakefile.lean
@@ -2,7 +2,7 @@ import Lake
 open Lake DSL
 
 package «oatp» where
-  version := v!"0.7.4"
+  version := v!"0.7.5"
   leanOptions := #[⟨`autoImplicit, false⟩, ⟨`relaxedAutoImplicit, false⟩]
 
 require grip from git
