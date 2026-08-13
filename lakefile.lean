72cc81e4a916ea9b83cd886b020fbf489ec9eb5b
Jonathan Cubides <prieto.jona@gmail.com>
2026-08-13 18:29:28 -0500

    build: pin newest first-party releases

--- lakefile.lean
+++ lakefile.lean
@@ -2,40 +2,44 @@ import Lake
 open Lake DSL
 
 package «oatp» where
-  version := v!"0.7.5"
+  version := v!"0.7.6"
   leanOptions := #[⟨`autoImplicit, false⟩, ⟨`relaxedAutoImplicit, false⟩]
 
 require grip from git
   "https://github.com/jonaprieto/lean-grip.git"
-  @ "v0.1.0"
+  @ "v0.3.4"
+
+require «grip-json» from git
+  "https://github.com/jonaprieto/lean-grip-json.git"
+  @ "v0.1.4"
 
 require tptp from git
   "https://github.com/jonaprieto/lean-grip-tptp.git"
-  @ "v0.5.1"
+  @ "v0.5.5"
 
 require «termcolor» from git
   "https://github.com/jonaprieto/lean-termcolor.git"
-  @ "v1.1.0"
+  @ "v1.1.5"
 
 require «termcolor-diagnostics» from git
   "https://github.com/jonaprieto/lean-termcolor-diagnostics.git"
-  @ "v0.1.13"
+  @ "v0.1.16"
 
 require «termcolor-terminal» from git
   "https://github.com/jonaprieto/lean-termcolor-terminal.git"
-  @ "v0.3.1"
+  @ "v0.3.6"
 
 require «termcolor-widgets» from git
   "https://github.com/jonaprieto/lean-termcolor-widgets.git"
-  @ "v0.1.9"
+  @ "v0.1.13"
 
 require «termcolor-repl» from git
   "https://github.com/jonaprieto/lean-termcolor-repl.git"
-  @ "v0.8.1"
+  @ "v0.8.6"
 
 require argus from git
   "https://github.com/jonaprieto/lean-argus.git"
-  @ "v0.5.0"
+  @ "v0.5.3"
 
 @[default_target]
 lean_lib «OATP» where
