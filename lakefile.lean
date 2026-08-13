d6b580f76582af7460c9fd5bfed5f39c062c21fc
Jonathan Cubides <prieto.jona@gmail.com>
2026-08-12 21:29:44 -0500

    build: adopt Lean v4.33.0

--- lakefile.lean
+++ lakefile.lean
@@ -2,7 +2,7 @@ import Lake
 open Lake DSL
 
 package «oatp» where
-  version := v!"0.7.1"
+  version := v!"0.7.2"
   leanOptions := #[⟨`autoImplicit, false⟩, ⟨`relaxedAutoImplicit, false⟩]
 
 require grip from git
