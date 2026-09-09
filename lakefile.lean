cc27ad626e30b181774777e46de67075391d741f
Jonathan Cubides <prieto.jona@gmail.com>
2026-09-09 14:35:55 -0500

    build: refresh application dependencies

--- lakefile.lean
+++ lakefile.lean
@@ -7,39 +7,39 @@ package «oatp» where
 
 require grip from git
   "https://github.com/jonaprieto/lean-grip.git"
-  @ "v0.3.5"
+  @ "v0.3.6"
 
 require «grip-json» from git
   "https://github.com/jonaprieto/lean-grip-json.git"
-  @ "v0.1.6"
+  @ "v0.1.7"
 
 require tptp from git
   "https://github.com/jonaprieto/lean-grip-tptp.git"
-  @ "v0.5.6"
+  @ "v0.5.7"
 
 require «termcolor» from git
   "https://github.com/jonaprieto/lean-termcolor.git"
-  @ "v1.1.6"
+  @ "v1.1.7"
 
 require «termcolor-diagnostics» from git
   "https://github.com/jonaprieto/lean-termcolor-diagnostics.git"
-  @ "v0.1.17"
+  @ "v0.1.18"
 
 require «termcolor-terminal» from git
   "https://github.com/jonaprieto/lean-termcolor-terminal.git"
-  @ "v0.3.7"
+  @ "v0.3.8"
 
 require «termcolor-widgets» from git
   "https://github.com/jonaprieto/lean-termcolor-widgets.git"
-  @ "v0.1.14"
+  @ "v0.1.15"
 
 require «termcolor-repl» from git
   "https://github.com/jonaprieto/lean-termcolor-repl.git"
-  @ "v0.8.7"
+  @ "v0.8.8"
 
 require argus from git
   "https://github.com/jonaprieto/lean-argus.git"
-  @ "v0.5.4"
+  @ "v0.5.5"
 
 @[default_target]
 lean_lib «OATP» where
